1f04574c195bfc60868a93a6b24c41669e944822
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-12-24 05:32:23 +0100

    make a changes

--- index.js
+++ index.js
@@ -1,41 +1,32 @@
 "use strict";
-
 require('dotenv').config();
-
 const http = require('http');
 const express = require('express');
-const multer = require("multer");
-
-// Multer for form-data and file uploads;
 
 const app = express();
-const port = process.env.port || 4400;
-
-const server =http.createServer(app);
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>{
-    return res.status(200).json({message: 'Tada!'})
-})
 
-app.post("/signup", (req,res)=>{
-    console.log(req.header('content-type'));
-    console.log(req.body);
-    console.log(JSON.stringify(req.body));
-    return res.status(200).json({message: 'Signup successful!'})
-})
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  console.log('Request Headers:', req.headers);
+  console.log('Request Body:', req.body);
+  next(); 
+});
 
-app.post("/login", multer().none(),  (req,res)=>{
-    console.log(req.body.name);
-    console.log(JSON.stringify(req.body));
-    return res.status(200).json({message: 'Login successful!'})
-})
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'Hello!' });
+});
 
-// server.timeout = 120_000;
+app.post('/send-names', (req, res) => {
+  const names = req.body.names || [];
+  console.log("Received names:", names);
+  return res.status(200).json({ message: 'Names received successfully', names });
+});
 
-server.listen(port, () =>{
-    console.log(process.env.port);
-    console.log(`App is live and running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
